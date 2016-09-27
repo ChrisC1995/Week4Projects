8b0416675e79512bada515cf0f14af15bd4e80d0
Christian Campbell <christian.campbell@interapthq.com>
2016-09-27 11:40:50 -0400

    Add files via upload

--- TipCalculator.js
+++ TipCalculator.js
@@ -26,6 +26,14 @@ function calculateTip(billAmount, service, split){
 var result = (+billAmount * +service / +split).toFixed(2);
 console.log(result);
 $("#totalTip").append("$" + result);
+if (split > 1){
+    var totalResult = ((+billAmount/+split) + +result).toFixed(2);
+    $("#totalBillAmount").append("$"+ totalResult);
+    }
+    else {
+       var totalSoloResult = (+billAmount + +result).toFixed(2);
+    $("#totalBillAmount").append("$" + totalSoloResult);
+    }
 $("#tipResult").fadeIn();
 $("#resetButton").fadeIn();
 $("#calculate").fadeOut();
